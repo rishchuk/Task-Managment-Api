9d8e51e7b587367b751228f5a0a5048d08a4159d
Roman Ishchuk <01165722@pw.edu.pl>
2025-03-25 14:33:42 +0100

    Reorganize project structure, move backend files to tasks_backend/, update settings.py to integrate Vue with Django backend

--- tasks_backend/task_manager_api/settings.py
+++ tasks_backend/task_manager_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -140,3 +142,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+
+CORS_ALLOW_CREDENTIALS = True
